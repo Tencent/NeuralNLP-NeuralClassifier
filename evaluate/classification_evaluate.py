2247d2cf849ac3bcdc092df130b000f12b1c6084
liqunhit <liqunhit@gmail.com>
2025-07-07 22:10:07 +0800

    modify copyright notice

--- evaluate/classification_evaluate.py
+++ evaluate/classification_evaluate.py
@@ -2,7 +2,7 @@
 #coding:utf-8
 """
 Tencent is pleased to support the open source community by making NeuralClassifier available.
-Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+Copyright (C) 2019 Tencent. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except in compliance
 with the License. You may obtain a copy of the License at
 http://opensource.org/licenses/MIT
